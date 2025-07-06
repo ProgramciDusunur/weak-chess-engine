1d147bf782d43635af003d64c019072a4300840d
Dragjon <140328303+Dragjon@users.noreply.github.com>
2025-07-07 04:06:36 +0800

    v0.1.12 Triple PVS
    --------------------------------------------------
    Results of WeakDev vs Weak (8+0.08, NULL, NULL, 8movesv3.pgn):
    Elo: 27.39 +/- 11.81, nElo: 32.46 +/- 13.93
    LOS: 100.00 %, DrawRatio: 33.81 %, PairsRatio: 1.33
    Games: 2390, Wins: 942, Losses: 754, Draws: 694, Points: 1289.0 (53.93 %)
    Ptnml(0-2): [117, 223, 404, 257, 194], WL/DD Ratio: 2.78
    LLR: 2.94 (100.0%) (-2.94, 2.94) [0.00, 5.00]
    --------------------------------------------------
    SPRT ([0.00, 5.00]) completed - H1 was accepted

--- src/search.cpp
+++ src/search.cpp
@@ -254,6 +254,11 @@ int32_t alpha_beta(Board &board, int32_t depth, int32_t alpha, int32_t beta, int
             score = -alpha_beta(board, depth + extension - 1, -beta, -alpha, ply + 1);
         else {
             score = -alpha_beta(board, depth - reduction + extension - 1, -alpha - 1, -alpha, ply + 1);
+
+            // Triple PVS
+            if (reduction > 0 && score > alpha)
+                score = -alpha_beta(board, depth + extension - 1, -alpha - 1, -alpha, ply + 1);
+
             // Research
             if (score > alpha && score < beta) {
                 score = -alpha_beta(board, depth + extension - 1, -beta, -alpha, ply + 1);
