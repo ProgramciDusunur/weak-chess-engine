efe6e2aea2f0637fe976ff11ab8e93e720cfe59c
Dragjon <140328303+Dragjon@users.noreply.github.com>
2025-07-12 13:04:08 +0800

    Implement rook on open files. Bench: 607230

--- src/bitboard.hpp
+++ src/bitboard.hpp
@@ -5,6 +5,8 @@ int32_t count(uint64_t bb);
 extern const uint64_t WHITE_PASSED_MASK[64];
 extern const uint64_t BLACK_PASSED_MASK[64];
 extern const uint64_t OUTER_2_SQ_RING_MASK[64];
+extern const uint64_t WHITE_AHEAD_MASK[64];
+extern const uint64_t BLACK_AHEAD_MASK[64];
 bool is_white_passed_pawn(int32_t square, uint64_t black_pawns);
 bool is_black_passed_pawn(int32_t square, uint64_t white_pawns);
 int32_t count_white_passed_pawns(uint64_t white_pawns, uint64_t black_pawns);
