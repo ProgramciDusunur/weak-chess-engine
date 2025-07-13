1f9040df75abe95ec1207f774117d1dda5330b18
Dragjon <140328303+Dragjon@users.noreply.github.com>
2025-07-13 13:57:42 +0800

    No improving. Bench: 587409

--- src/search.cpp
+++ src/search.cpp
@@ -221,7 +221,7 @@ int32_t alpha_beta(Board &board, int32_t depth, int32_t alpha, int32_t beta, int
     int32_t static_eval = evaluate(board);
 
     // Improving heuristic (Whether we are at a better position than 2 plies before)
-    bool improving = static_eval > search_info.parent_parent_eval && search_info.parent_parent_eval != -100000;
+    // bool improving = static_eval > search_info.parent_parent_eval && search_info.parent_parent_eval != -100000;
 
     // Reverse futility pruning / Static Null Move Pruning
     // If eval is well above beta, we assume that it will hold
@@ -321,7 +321,7 @@ int32_t alpha_beta(Board &board, int32_t depth, int32_t alpha, int32_t beta, int
         // best moves at the start
         if (!is_noisy_move && depth >= late_move_reduction_depth.current){
             reduction += (int32_t)(((double)late_move_reduction_base.current / 100) + (((double)late_move_reduction_multiplier.current * log(depth) * log(move_count)) / 100));
-            reduction += (int32_t)!improving;
+            // reduction += (int32_t)!improving;
         }
 
         // Static Exchange Evaluation Pruning
@@ -351,8 +351,8 @@ int32_t alpha_beta(Board &board, int32_t depth, int32_t alpha, int32_t beta, int
         info.parent_parent_move_square = parent_move_square;
         info.parent_move_piece = move_piece;
         info.parent_move_square = to;
-        info.parent_parent_eval = info.parent_static_eval;
-        info.parent_static_eval = static_eval;
+        // info.parent_parent_eval = info.parent_static_eval;
+        // info.parent_static_eval = static_eval;
 
         // Principle Variation Search
         if (move_count == 1)
