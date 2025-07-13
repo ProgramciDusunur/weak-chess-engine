41164455131a739ca3187b2db2c108eed4ada61d
Dragjon <140328303+Dragjon@users.noreply.github.com>
2025-07-13 19:08:58 +0800

    Bench: 542432

--- src/bitboard.hpp
+++ src/bitboard.hpp
@@ -7,6 +7,8 @@ extern const uint64_t BLACK_PASSED_MASK[64];
 extern const uint64_t OUTER_2_SQ_RING_MASK[64];
 extern const uint64_t WHITE_AHEAD_MASK[64];
 extern const uint64_t BLACK_AHEAD_MASK[64];
+extern const uint64_t WHITE_FRONT_MASK[64];
+extern const uint64_t BLACK_FRONT_MASK[64];
 bool is_white_passed_pawn(int32_t square, uint64_t black_pawns);
 bool is_black_passed_pawn(int32_t square, uint64_t white_pawns);
 int32_t count_white_passed_pawns(uint64_t white_pawns, uint64_t black_pawns);
